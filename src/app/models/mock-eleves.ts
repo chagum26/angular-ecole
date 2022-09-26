f8011a30c7fc890c6a5de50012211d7ee5ba45fc
chagum26 <lievrehugo26230@gmail.com>
2022-09-26 09:31:54 +0200

    test

---
+++ src/app/models/mock-eleves.ts
@@ -0,0 +1,9 @@
+import { eleve } from './eleve';
+
+export const ELEVES: eleve[] = [
+  { id: 12, nom: 'Hugo LIEVRE', classe: '3A' },
+  { id: 13, nom: 'Benj', classe: '5A' },
+  { id: 14, nom: 'Ako', classe: '3B' },
+  { id: 15, nom: 'Jeanne', classe: '2C' },
+  { id: 16, nom: 'Aurore', classe: '1F' }
+];
